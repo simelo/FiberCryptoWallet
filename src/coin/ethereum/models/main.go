46e92eca36e1100ab7a685534e876dc3bfc3e8f4
Mauricio Perdomo <ml.perdomo97@gmail.com>
2020-02-08 19:31:06 -0500

    [eth][plugin] Complete methods set of AltcoinPlugin interface in EthereumPlugin type

--- src/coin/ethereum/models/main.go
+++ src/coin/ethereum/models/main.go
@@ -1,8 +1,11 @@
 package ethereum
 
 import (
+	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/fibercrypto/fibercryptowallet/src/coin/ethereum/config"
 	"github.com/fibercrypto/fibercryptowallet/src/core"
+	"github.com/ethereum/go-ethereum/core/types"
+
 )
 
 const (
@@ -59,4 +62,29 @@ func (p *EthereumPlugin) LoadWalletEnvs []core.WalletEnv{
 		}
 	}
 	return wltEnvs
+}
+
+//TODO waiting for PEX implementation in ethereum plugin
+func (p *EthereumPlugin) LoadPEX(netType string) (core.PEX, error){
+	return nil, nil
+}
+
+//TODO waiting for Address implementation in ethereum plugin
+func (p *EthereumPlugin) AddressFromString(addrStr) (core.Address, error){
+	return nil, nil
+}
+
+
+//TODO waiting for PubKey implementation in ethereum plugin
+func (p *EthereumPlugin) PubKeyFromBytes(b []byte) (core.PubKey, error){
+	return nil, nil
+}
+
+//TODO waiting for SecKey implementation in ethereum plugin
+func (p *EthereumPlugin) SecKeyFromBytes(b []byte) (core.SecKey, error){
+	return nil, nil
+}
+
+func NewEthereumPlugin() core.AltcoinPlugin{
+	return &EthereumPlugin{}
 }
