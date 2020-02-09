17f8d857cb071d1dce6ab18daa6ec16772a71b46
Mauricio Perdomo <ml.perdomo97@gmail.com>
2020-02-08 19:12:11 -0500

    [eth][config] Add wallet env settings

--- src/coin/ethereum/config/config.go
+++ src/coin/ethereum/config/config.go
@@ -5,11 +5,14 @@ import (
 	"strings"
 
 	local "github.com/fibercrypto/fibercryptowallet/src/main"
+	"github.com/fibercrypto/fibercryptowallet/src/util"
 )
 
 const (
-	SettingPathToNode = "node"
-	SectionName       = "ethereum"
+	SettingPathToNode         = "node"
+	SettingPathToWalletSource = "walletSource"
+	SectionName               = "ethereum"
+	LocalWallet               = "local"
 )
 
 var (
@@ -26,7 +29,20 @@ func RegisterConfig() error {
 
 	nodeOpt := local.NewOption(SettingPathToNode, []string{}, false, string(nodeBytes))
 
-	sectionManager = cm.RegisterSection(SectionName, []*local.Option{nodeOpt})
+	walletsDefaultDirectory := util.GetMultiPlatformUserDirectory([]string{".ethereum", "wallets"})
+	wltSrc := &walletSource{
+		id:     "1",
+		Tp:     string(LocalWallet),
+		Source: walletsDefaultDirectory,
+	}
+	wltSrcBytes, err := json.Marshal(wltSrc)
+	if err != nil {
+		return err
+	}
+
+	wltOpt := local.NewOption(string(wltSrc.id), []string{SettingPathToWalletSource}, false, string(wltSrcBytes))
+
+	sectionManager = cm.RegisterSection(SectionName, []*local.Option{nodeOpt, wltOpt})
 
 	return nil
 
@@ -35,4 +51,30 @@ func RegisterConfig() error {
 func GetOption(path string) (string, error) {
 	stringList := strings.Split(path, "/")
 	return sectionManager.GetValue(stringList[len(stringList)-1], stringList[:len(stringList)-1])
+}
+
+func GetWalletSources() ([]*walletSource, error) {
+	wltsString, err := getValues(SettingPathToWalletSource)
+	if err != nil {
+		return nil, err
+	}
+	wltSrcs := make([]*walletSource, len(wltsString))
+	for i, wlt := range wltsString {
+		wltSrcs[i] = new(walletSource)
+		err = json.Unmarshal([]byte(wlt), wltSrcs[i])
+		if err != nil {
+			return nil, err
+		}
+	}
+	return wltSrcs, nil
+}
+
+type walletSource struct {
+	id     string
+	Tp     string `json:"SourceType"`
+	Source string `json:"Source"`
+}
+
+func getValues(prefix string) ([]string, error) {
+	return sectionManager.GetValues(strings.Split(prefix, "/"))
 }
