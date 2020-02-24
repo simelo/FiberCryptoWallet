9b2470e007ac408c38830f641cede156c0fba340
Mauricio Perdomo <ml.perdomo97@gmail.com>
2020-02-23 20:32:19 -0500

    [eth][test] Add test for WalletDirectory.GetStorage method

---
+++ src/coin/ethereum/models/wallet_test.go
@@ -0,0 +1,25 @@
+package ethereum
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestWalletDirectoryGetStorage(t *testing.T) {
+	dir := "wallet-dir"
+	test := []struct {
+		wlt  *WalletsDirectory
+		want *WalletsDirectory
+	}{
+		{wlt: NewWalletDirectory(dir), want: NewWalletDirectory(dir)},
+	}
+
+	for i, tt := range test {
+		t.Run(fmt.Sprintf("GetStorage_%d", i), func(t *testing.T) {
+			storage := tt.wlt.GetStorage()
+			require.Equal(t, tt.want, storage)
+		})
+	}
+}
